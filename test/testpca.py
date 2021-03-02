97b82e76488711a37f4c7ffb528d93777ad127af
madsams <39955233+madsams@users.noreply.github.com>
2021-03-02 09:17:20 +0330

    sub_series added

---
+++ test/testpca.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal, assert_series_equal
+
+from pca import sub_series
+
+
+class TestSubSeries(unittest.TestCase):
+    def test_empty(self):
+        sr = pd.Series(dtype='float64')
+        exp = sr
+        assert_series_equal(exp, sub_series(sr), check_dtype=False)
+
+    def test1(self):
+        sr = pd.Series([3, 4, 1, 2, 0])  # mean = 2
+        exp = pd.Series([1, 2, -1, 0, -2])  # mean = 20
+        assert_series_equal(exp, sub_series(sr), check_dtype=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
